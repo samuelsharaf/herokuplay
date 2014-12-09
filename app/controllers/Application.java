89cff18b4c827f9cf09502e1176017f9393ba6e4
Samuel Sharaf <samuel.sharaf@salesforce.com>
2014-12-09 13:28:34 -0800

    fsfs

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -27,7 +27,7 @@ public class Application extends Controller {
 ///////////
 	try {
         
-		String rand = new Integer(new Random().intValue()).toString();
+		String rand = new Integer(new Random().nextInt()).toString();
 		Connection connection = getConnection();
       	Statement stmt = connection.createStatement();
 		String query = "insert into public.hellonames (ID, Name) values(" + rand + ","  + myName + ")";
